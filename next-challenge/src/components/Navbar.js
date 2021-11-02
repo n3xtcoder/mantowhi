17d31e655819c6ef72c0645924c7a407d0a551b0
Laura <65158950+lauratro@users.noreply.github.com>
2021-11-02 11:13:38 +0100

    Navbar links to diff. parts of the project

---
+++ next-challenge/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import { Nav, Container } from "react-bootstrap";
+import { BrowserRouter as Router, Link, useParams } from "react-router-dom";
+import "../App.css";
+
+export default function NavbarApp() {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Link to={`/`}>
+            <Navbar.Brand className="textNav">N3xtCoder Challenge</Navbar.Brand>
+          </Link>
+          <Nav className="me-auto">
+            <Link className="mr-3" to={`/`}>
+              Photo Album
+            </Link>
+            <Link href="#features">Aws Upload</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
