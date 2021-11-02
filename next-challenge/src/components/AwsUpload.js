9fb267945a9c77eea9fd322ace241313966b9940
Laura <65158950+lauratro@users.noreply.github.com>
2021-11-02 16:59:10 +0100

    Progress bar defined

--- next-challenge/src/components/AwsUpload.js
+++ next-challenge/src/components/AwsUpload.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
+import FileUploadProgress from "react-fileupload-progress";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import "../App.css";
 //import S3FileUpload from "react-s3";
 require("dotenv").config();
 const AWS = require("aws-sdk");
 
 export default function AwsUpload() {
   const [photoUploaded, setPhotoUploaded] = useState([]);
-
+  const [awsFile, setAwsFile] = useState({});
+  const [fileuploading, setFileUploading] = useState({});
+  const [uploadConfirm, setUploadConfirm] = useState(false);
+  console.log(awsFile);
   console.log(photoUploaded);
 
   const fileSize = photoUploaded.size / 1024 / 1024;
@@ -32,28 +38,53 @@ export default function AwsUpload() {
     return s3.upload(params, function (err, data) {
       if (err) {
         console.log("There was an error uploading your file: ", err);
+        setUploadConfirm(false);
         return false;
       }
       console.log("Successfully uploaded file.", data);
+      setAwsFile(data);
+      setUploadConfirm(true);
       return true;
     });
   };
+
   let uploadPic2 = () => {
-    uploadFile(photoUploaded, photoUploaded.name, photoUploaded.type);
+    uploadFile(photoUploaded, photoUploaded.name, photoUploaded.type).on(
+      "httpUploadProgress",
+      (progress) => {
+        let uploaded = parseInt((progress.loaded * 100) / progress.total);
+        console.log(uploaded);
+        setFileUploading({
+          progress: uploaded,
+          uploadText: "Uploading...",
+          uploading: true,
+        });
+      }
+    );
   };
 
   return (
-    <div>
-      <input
-        type="file"
-        onChange={(e) => {
-          setPhotoUploaded(e.target.files[0]);
-        }}
-      />
-      <button disabled={fileSize > 50 ? true : false} onClick={uploadPic2}>
+    <div className="d-flex flex-column justify-content-center ">
+      <div className="d-flex justify-content-center w-75">
+        <input
+          type="file"
+          onChange={(e) => {
+            setPhotoUploaded(e.target.files[0]);
+          }}
+        />
+      </div>
+      <button
+        className="w-20"
+        disabled={fileSize > 50 ? true : false}
+        onClick={uploadPic2}
+      >
         Add file
       </button>
+
+      <ProgressBar animated now={fileuploading.progress} />
+      {fileuploading.progress && <p>{`${fileuploading.progress}%`}</p>}
       {fileSize > 50 && <p>The file must be smaller than 50 MB</p>}
+      {uploadConfirm && <p>Successfully upload file</p>}
     </div>
   );
 }
