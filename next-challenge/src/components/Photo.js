58a442fe2c6b7bfc3f50072b1bffe252898d92aa
Laura <65158950+lauratro@users.noreply.github.com>
2021-11-02 11:12:29 +0100

    Comp. to show single photo and switch to others

---
+++ next-challenge/src/components/Photo.js
@@ -0,0 +1,41 @@
+import React, { useState, useContext } from "react";
+
+import { VariablesContext } from "../context/VariablesContext";
+import { BrowserRouter as Router, Link, useParams } from "react-router-dom";
+import "../App.css";
+
+export default function Photo() {
+  let { id } = useParams();
+  let nextId = () => {
+    return Number(id) + 1;
+  };
+  let prevId = () => {
+    return Number(id) - 1;
+  };
+
+  const { photoAlbum, setPhotoAlbum } = useContext(VariablesContext);
+
+  return (
+    <div>
+      <div className="d-flex justify-content-center m-1  ">
+        {photoAlbum.map((photo) => {
+          if (photo.id == id) {
+            return <img className="photoMobile" src={photo.url} />;
+          }
+        })}
+      </div>
+      <div className="buttonSwitch">
+        <Link to={`${prevId()}`}>
+          <button className="buttonLR" disabled={id === "1" ? true : false}>
+            Prev
+          </button>
+        </Link>
+        <Link to={`${nextId()}`}>
+          <button className="buttonLR" disabled={id === "50" ? true : false}>
+            Next
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
